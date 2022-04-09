90c4dc8f460c54b31ef7d9d6c15f9ddb33cbcab1
iano <ianmwema07@gmail.com>
2022-04-09 12:20:08 +0300

    add quote-details component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,17 @@
 export class Quote {
+  id: number;
+    quote: string;
+    author: string;
+    name: string;
+    upvote: number;
+    downvote: number;
+
+    constructor(id:number, quote:string, author:string, name:string, upvote:number, downvote:number ) {
+        this.id = id;
+        this.quote = quote;
+        this.author = author;
+        this.name = name;
+        this.upvote = upvote;
+        this.downvote = downvote;
+    }
 }
