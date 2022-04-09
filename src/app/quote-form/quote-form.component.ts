a7f26675dacfe3ed53a1e37acb69da1ecedb0b75
iano <ianmwema07@gmail.com>
2022-04-09 18:28:43 +0300

    add eventemitter on quote-form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -9,7 +9,11 @@ import { Quote } from '../quote';
 export class QuoteFormComponent implements OnInit {
 
   newQuote = new Quote(0,"","","",0,0,new Date);
+  @Output() addGoal = new EventEmitter<Quote>();
 
+  submitQuote(){
+this.addGoal.emit(this.newQuote);
+  }
   constructor() { }
 
   ngOnInit(): void {
