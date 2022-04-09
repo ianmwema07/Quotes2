17ec740e9e98802b03a0d1c53297ba300052104a
iano <ianmwema07@gmail.com>
2022-04-09 22:31:54 +0300

    add directive

--- src/app/highest-upvote.directive.ts
+++ src/app/highest-upvote.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighestUpvote]'
 })
 export class HighestUpvoteDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { 
+    elem.nativeElement.style.backgroundColor = "blue"
+  }
 
 }
