a3d95890d2cd7ecb74833b71d5472fcb49ac609a
iano <ianmwema07@gmail.com>
2022-04-10 12:19:30 +0300

    Auto-generated commit [ci skip]

--- src/app/day-count.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DayCountPipe } from './day-count.pipe';
-
-describe('DayCountPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DayCountPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
